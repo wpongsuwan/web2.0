1d99e10b39899c866c35e0d0c4d102d091b0ec2d
wpongsuwan <wpongsuwan@gmail.com>
2018-08-06 13:27:22 +0700

    Posting with Postman

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
- res.send('Hello Web 2.0');
-})
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+  var email = req.body.email;
+  var amount = req.body.amount;
+  res.send({'amount' : amount, 'email' : email });
+
+});
 
 app.listen(3000, () => {
    console.log('server is running on port 3000');
